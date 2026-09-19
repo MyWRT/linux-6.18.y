fb610a3faa1b99718c15e3544e40dc85204995e2
ophub <68696949+ophub@users.noreply.github.com>
2026-09-19 14:21:11 +0800

    mt7668: fix -Wstringop-overread in wext_set_encode_ext
    Pass the actual buffer size to wext_set_encode_ext and validate
    key_len against it before copying from the flexible array member.
    The caller's previous cap of sizeof(struct iw_encode_ext) left
    zero bytes for key data at offset 40, causing GCC to flag the
    read as out-of-bounds.

--- drivers/net/wireless/mediatek/mt7668/os/linux/gl_wext.c
+++ drivers/net/wireless/mediatek/mt7668/os/linux/gl_wext.c
@@ -2697,7 +2697,8 @@ UINT_8 keyStructBuf[100];	/* add/remove key shared buffer */
 
 static int
 wext_set_encode_ext(IN struct net_device *prNetDev,
-		    IN struct iw_request_info *prIwrInfo, IN struct iw_point *prEnc, IN char *pcExtra)
+		    IN struct iw_request_info *prIwrInfo, IN struct iw_point *prEnc, IN char *pcExtra,
+		    IN UINT_32 u4ExtraSize)
 {
 	P_PARAM_REMOVE_KEY_T prRemoveKey = (P_PARAM_REMOVE_KEY_T) keyStructBuf;
 	P_PARAM_KEY_T prKey = (P_PARAM_KEY_T) keyStructBuf;
@@ -2822,7 +2823,8 @@ wext_set_encode_ext(IN struct net_device *prNetDev,
 				prWepKey->u4KeyLength = prIWEncExt->key_len;
 				if (prIWEncExt->key_len > sizeof(prWepKey->aucKeyMaterial))
 					return -EINVAL;
-				/* kalMemCopy(prWepKey->aucKeyMaterial, pcExtra, prIWEncExt->key_len); */
+				if (u4ExtraSize < offsetof(struct iw_encode_ext, key) + prIWEncExt->key_len)
+					return -EINVAL;
 				kalMemCopy(prWepKey->aucKeyMaterial, prIWEncExt->key, prIWEncExt->key_len);
 
 				rStatus = kalIoctl(prGlueInfo,
@@ -3429,7 +3431,8 @@ int wext_support_ioctl(IN struct net_device *prDev, IN struct ifreq *prIfReq, IN
 		if (iwr->u.encoding.pointer) {
 			u4ExtraSize = iwr->u.encoding.length;
 
-			if (u4ExtraSize > sizeof(struct iw_encode_ext)) {
+			if (u4ExtraSize < offsetof(struct iw_encode_ext, key) ||
+			    u4ExtraSize > offsetof(struct iw_encode_ext, key) + 32) {
 				ret = -EINVAL;
 				break;
 			}
@@ -3448,7 +3451,7 @@ int wext_support_ioctl(IN struct net_device *prDev, IN struct ifreq *prIfReq, IN
 		}
 
 		if (ret == 0)
-			ret = wext_set_encode_ext(prDev, NULL, &iwr->u.encoding, prExtraBuf);
+			ret = wext_set_encode_ext(prDev, NULL, &iwr->u.encoding, prExtraBuf, u4ExtraSize);
 
 		if (prExtraBuf) {
 			kalMemFree(prExtraBuf, VIR_MEM_TYPE, u4ExtraSize);
